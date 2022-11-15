fb63ef232469f6e3d627e01455ec5f0ae27d55da
Listopad02 <123ayetvbass@gmail.com>
2022-11-15 15:59:38 +0300

    search bar styles added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Box } from "@mui/material"
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components"
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} exact />
+        <Route path="/video/:id" element={<VideoDetail />} exact />
+        <Route path="/channel/:id" element={<ChannelDetail />} exact />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} exact />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+)
 
 export default App
